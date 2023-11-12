c8336be6629c7d6ef59c5e1629fa64bdf70eb6dd
DJ_Lumiere <djminstar573@gmail.com>
2023-11-12 13:11:58 +0900

    Update Application.kt
    feat: 주문이 입력으로 들어오면 파싱하는 기능 추가
    
    메뉴와 메뉴 사이는 ","로 구분되게, 메뉴와 수량 사이에는 "-"로 구분되게 구현

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,15 @@
 package christmas
 
+fun parseOrder(input: String): Map<String, Int> {
+    val order: MutableMap<String, Int> = mutableMapOf()
+    val menus: List<String> = input.split(",")
+    for (menu in menus) {
+        val (item, quantity) = menu.split("-")
+        order[item] = quantity.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+    }
+    return order
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
