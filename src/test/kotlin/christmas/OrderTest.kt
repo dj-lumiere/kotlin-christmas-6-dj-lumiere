44db6982c315bf9eab99820a8b3659ea1907a5fd
DJ_Lumiere <djminstar573@gmail.com>
2023-11-15 17:34:15 +0900

    Test : addItem 메소드 테스트하는 케이스 추가

---
+++ src/test/kotlin/christmas/OrderTest.kt
@@ -0,0 +1,28 @@
+package christmas
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class OrderTest{
+    @Test
+    fun `중복된 메뉴를 넣을 때 에러 발생`(){
+        val menuitem = RestaurantMenu().menuItems
+        val order = Order()
+        order.addItem(menuitem.getValue("양송이수프"), 1)
+        assertThrows<IllegalArgumentException> { order.addItem(menuitem.getValue("양송이수프"), 1) }
+    }
+
+    @Test
+    fun `잘못된 수량을 넣을 때 에러 발생`(){
+        val menuitem = RestaurantMenu().menuItems
+        val order = Order()
+        assertThrows<IllegalArgumentException> { order.addItem(menuitem.getValue("양송이수프"), -1) }
+    }
+
+    @Test
+    fun `너무 많은 양을 넣을 때 에러 발생`(){
+        val menuitem = RestaurantMenu().menuItems
+        val order = Order()
+        assertThrows<IllegalArgumentException> { order.addItem(menuitem.getValue("양송이수프"), 21) }
+    }
+}
